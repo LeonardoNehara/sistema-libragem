f14fd5a443700451c91e557265d0dc53e775d38c
Leonardo Nehara <leonardonehara@gmail.com>
2026-05-20 10:38:16 -0300

    feat: adiciona migrations iniciais do sistema

---
+++ database/migrations/2026_05_19_161044_create_checklist_pneus_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checklist_pneus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('checklist_id')
+                ->constrained('checklists_calibragem')
+                ->onDelete('cascade');
+
+            $table->integer('posicao'); // Ex: 1, 2, 3, 4...
+            $table->integer('libragem'); // Ex: 110, 120, 125
+
+            $table->enum('status', [
+                'calibrado',
+                'baixo',
+                'alto',
+                'critico'
+            ])->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checklist_pneus');
+    }
+};
